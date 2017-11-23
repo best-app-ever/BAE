7efba5da90ca2cc5d2d55084f7f358465bae0b24
Ashutosh <ashutosh.singh@decathlon.com>
2017-11-23 16:25:03 +0100

    adding customer page

---
+++ app/src/main/java/com/decathlon/bae/bae/CustomerActivity.java
@@ -0,0 +1,17 @@
+package com.decathlon.bae.bae;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Ashutosh on 23/11/2017.
+ */
+
+public class CustomerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer);
+    }
+}
