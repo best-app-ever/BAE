1c60664d074742bd82fbc41cea9049be7178616d
Ashutosh <ashutosh.singh@decathlon.com>
2017-11-24 18:53:44 +0100

    design of creation page

--- app/src/main/java/com/decathlon/bae/bae/CreationActivity.java
+++ app/src/main/java/com/decathlon/bae/bae/CreationActivity.java
@@ -1,8 +1,17 @@
 package com.decathlon.bae.bae;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by Ashutosh on 23/11/2017.
  */
 
-public class CreationActivity {
+public class CreationActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creation);
+    }
 }
--- app/src/main/java/com/decathlon/bae/bae/CustomerActivity.java
+++ app/src/main/java/com/decathlon/bae/bae/CustomerActivity.java
@@ -1,7 +1,10 @@
 package com.decathlon.bae.bae;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Ashutosh on 23/11/2017.
@@ -13,5 +16,13 @@ public class CustomerActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer);
+        Button button = (Button) findViewById(R.id.button4);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CustomerActivity.this,CreationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
