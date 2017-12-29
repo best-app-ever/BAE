416d37ee738aaf5f64fe9a773e574367e00b6ceb
Ashutosh <ashutosh.singh@decathlon.com>
2017-12-29 11:24:43 +0100

    design of login page done

--- app/src/main/java/com/decathlon/bae/bae/LoginActivity.java
+++ app/src/main/java/com/decathlon/bae/bae/LoginActivity.java
@@ -1,17 +1,55 @@
 package com.decathlon.bae.bae;
 
 import android.app.Activity;
-import android.drm.DrmStore;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
 
 /**
  * Created by Ashutosh on 06/12/2017.
  */
 
 public class LoginActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.actitvity_login);
+
+        Button but = (Button)findViewById(R.id.button);
+        final RequestQueue queue = Volley.newRequestQueue(this);
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final TextView mTextView;
+
+                mTextView = (TextView) findViewById(R.id.textView7);
+
+
+
+             }
+
+
+
+         });
     }
 }
---
+++ app/src/main/java/com/decathlon/bae/bae/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.decathlon.bae.bae;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Ashutosh on 19/12/2017.
+ */
+
+public class ProfileActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
